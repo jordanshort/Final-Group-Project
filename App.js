3f0b7e1fe186ef72f593bee9f46a538a0da6ba0d
Brandon Allred <b32alls@gmail.com>
2018-03-23 19:07:22 -0600

    Removing all the RNAppAuth dependencies that were causing build issues

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-import AppAuth from 'react-native-app-auth'
 import axios from 'axios';
 
 import { auth0, AUTH0_DOMAIN } from './src/auth0'
@@ -11,16 +10,14 @@ export default class App extends React.Component {
       .webAuth
       .authorize({scope: 'openid profile email', audience: `https://${AUTH0_DOMAIN}/userinfo`, useBrowser: true, responseType:'id_token'})
       .then(credentials => {
-        // console.log(verifyToken)
-        verifyToken(credentials);
         console.log(credentials)
-        axios.post(`/api/index/${credentials}`).then(res=>{
-          console.log(res)
-          res.status(200).send(res)
-        })
+        // axios.post(`/api/index/${credentials}`).then(res=>{
+        //   console.log(res)
+        //   res.status(200).send(res)
+        // })
         
       })
-      .catch(error => console.log(error));
+      // .catch(error => console.log(error));
 
   }
   render() {
--- android/app/src/main/java/com/coolendesk/MainApplication.java
+++ android/app/src/main/java/com/coolendesk/MainApplication.java
@@ -3,7 +3,6 @@ package com.coolendesk;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNAppAuthPackage;
 import com.auth0.react.A0Auth0Package;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAppAuthPackage(),
             new A0Auth0Package()
       );
     }
